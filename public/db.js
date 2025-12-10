4a651155573cfee79d8aef0bce5fb0c7f83726b8
rigoxjz <rigo2004j@gmail.com>
2025-12-10 17:11:54 -0600

    Create db.js

---
+++ public/db.js
@@ -0,0 +1,14 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: { rejectUnauthorized: false }
+});
