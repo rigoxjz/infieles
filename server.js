8dc07d70315a5b44a081721ebe65699b7927f888
rigoxjz <rigo2004j@gmail.com>
2025-12-10 21:26:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from "express";
 import cors from "cors";
 import { pool } from "./db.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 
 const app = express();
 app.use(cors());
