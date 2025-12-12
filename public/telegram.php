791ddd33b1c1c1ad026513028c66891bde6a9484
rigoxjz <rigo2004j@gmail.com>
2025-12-12 14:57:03 -0600

    Update telegram.php

--- public/telegram.php
+++ public/telegram.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/config.php';
-
 function send_to_telegram($msg) {
     $url = "https://api.telegram.org/bot" . TELEGRAM_BOT_TOKEN . "/sendMessage";
     $data = [
@@ -10,12 +9,12 @@ function send_to_telegram($msg) {
     ];
     $options = [
         "http" => [
-            "header"  => "Content-Type: application/x-www-form-urlencoded\r\n",
-            "method"  => "POST",
+            "header" => "Content-Type: application/x-www-form-urlencoded\r\n",
+            "method" => "POST",
             "content" => http_build_query($data),
         ]
     ];
-    $context  = stream_context_create($options);
+    $context = stream_context_create($options);
     return file_get_contents($url, false, $context);
 }
 ?>
